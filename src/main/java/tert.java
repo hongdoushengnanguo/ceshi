26e460b600025a5d5718124b9e64a9f59b995020
李修坤 <lxkbbd@163.com>
2019-11-26 14:35:37 +0800

    33.3

--- src/main/java/tert.java
+++ src/main/java/tert.java
@@ -7,4 +7,35 @@ public class tert {
     private String add;
     private String add1;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAdd() {
+        return add;
+    }
+
+    public void setAdd(String add) {
+        this.add = add;
+    }
+
+    public String getAdd1() {
+        return add1;
+    }
+
+    public void setAdd1(String add1) {
+        this.add1 = add1;
+    }
 }
